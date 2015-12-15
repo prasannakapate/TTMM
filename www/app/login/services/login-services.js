3ef70ea38fdd812e4278f9f4d6418d2cdb3358e2
Prasanna Kapate <prasanna.1616@gmail.com>
2015-12-15 17:10:08 +0530

    user login implemented

--- www/app/login/services/login-services.js
+++ www/app/login/services/login-services.js
@@ -10,27 +10,28 @@
 
         var LoginServices = {
             loginUser: loginUser
-        }
+        };
         return LoginServices;
 
         function loginUser(username, password) {
             var deffered = $q.defer();
             $http.get('https://api.parse.com/1/login', {
-                header: {
-                    'X-Parse-Application-Id': key.appid,
-                    'X-Parse-REST-API-Key': key.restid
-                },
-                params: {
-                    "username": "prasanna",
-                    "password": "prasanna"
-                }
-            }).success(function(response) {
-                console.log("user login Successfully", response.username, response.secrete);
-                deffered.resolve(response);
-            }).error(function(error, status) {
-                console.log("user login Error", error, " Status ", status);
-                deffered.reject(error, status);
-            })
+                    headers: {
+                        'X-Parse-Application-Id': key.appid,
+                        'X-Parse-REST-API-Key': key.restid
+                    },
+                    params: {
+                        username: username,
+                        password: password
+                    }
+                })
+                .success(function(response) {
+                    console.log("user login Successfully", response.username);
+                    deffered.resolve(response);
+                }).error(function(error, status) {
+                    console.log("user login Error", error, " Status ", status);
+                    deffered.reject(error, status);
+                });
             return deffered.promise;
         }
     }
--- www/app/settings.js
+++ www/app/settings.js
@@ -3,4 +3,4 @@
 var key = {
     appid: "mF8l9ob91ObHNIgzfX8tnzD8WKcRJGyUkYxm45o4",
     restid: "l8qZLpxhTxZWZYmfJZJu3r5fvwcJcHGYTe7w6igT"
-}
+};
