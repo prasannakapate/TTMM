18d12ae8a2eade06807961f141ff356db70486ed
Prasanna Kapate <prasanna.1616@gmail.com>
2016-02-16 14:19:50 +0530

    some cleanup

--- www/app/login/login-ctrl.js
+++ www/app/login/login-ctrl.js
@@ -25,7 +25,7 @@
 
         //Login for users
         function userLogin(credentials) {     
-        console.log('credentials', credentials);
+        //console.log('credentials', credentials);
             if (credentials && credentials.email && credentials.password) {
                 $ionicLoading.show({
                     template: 'Signing In...'
@@ -50,45 +50,5 @@
 
             }
         }
-
-        /*// FB Login
-        function fbLogin() {
-            FB.login(function(response) {
-                if (response.authResponse) {
-                    getUserInfo();
-                } else {
-                    console.log('User cancelled login or did not fully authorize.');
-                }
-            }, {
-                scope: 'email,user_photos,user_videos'
-            });
-
-            function getUserInfo() {
-                // get basic info
-                FB.api('/me', function(response) {
-                    console.log('Facebook Login RESPONSE: ' + angular.toJson(response));
-                    // get profile picture
-                    FB.api('/me/picture?type=normal', function(picResponse) {
-                        console.log('Facebook Login RESPONSE: ' + picResponse.data.url);
-                        response.imageUrl = picResponse.data.url;
-                        // store data to DB - Call to API
-                        // Todo
-                        // After posting user data to server successfully store user data locally
-                        var user = {};
-                        user.name = response.name;
-                        user.email = response.email;
-                        if (response.gender) {
-                            response.gender.toString().toLowerCase() === 'male' ? user.gender = 'M' : user.gender = 'F';
-                        } else {
-                            user.gender = '';
-                        }
-                        user.profilePic = picResponse.data.url;
-                        $cookieStore.put('userInfo', user);
-                        $state.go('tab.makeExpense');
-
-                    });
-                });
-            }
-        }*/
     }
 })();
